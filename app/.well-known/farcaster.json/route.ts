0d65df628bb8bd262e22a4ade8fe0b22849d5dee
0xjaywins <jaytech639@gmail.com>
2025-05-11 12:45:51 +0100

    added .env for embed

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import { APP_URL } from "../../../lib/constants";
 
+
 export async function GET() {
   const farcasterConfig = {
     accountAssociation: {
@@ -24,7 +25,7 @@ export async function GET() {
       tagline: "Vote on fun polls!",
       ogTitle: "Mini Poll App",
       ogDescription: "Compare dApps, tokens, and NFTs in the Monad ecosystem within Warpcast.",
-      ogImageUrl: "https://mini-poll-app.vercel.app/opengraph-image.png"
+      ogImageUrl: "https://mini-poll-app.vercel.app/.png"
     },
   };
 
